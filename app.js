e99f622e83ac787dd52a25bc7d1f5d13bc832ea1
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2021-08-18 18:40:58 +0100

    can slide image up

---
+++ app.js
@@ -0,0 +1,48 @@
+function init() {
+  const slides = document.querySelectorAll(".slide");
+  const pages = document.querySelectorAll(".page");
+  const backgrounds = [
+    `radial-gradient(#2B3760, #0B1023)`,
+    `radial-gradient(#4E3022, #161616)`,
+    `radial-gradient(#4E4342, #161616]`,
+  ];
+
+  let current = 0;
+
+  slides.forEach((slide, index) => {
+    slide.addEventListener("click", function () {
+      changeDots(this);
+      nextSlide(index);
+    });
+  });
+
+  function changeDots(dot) {
+    slides.forEach((slide) => {
+      slide.classList.remove("active");
+    });
+    dot.classList.add("active");
+  }
+
+  function nextSlide(pageNumber) {
+    const nextPage = pages[pageNumber];
+    const currentPage = pages[current];
+    const nextLeft = nextPage.querySelector(".hero .model-left");
+    const nextRight = nextPage.querySelector(".hero .model-right");
+    const currentLeft = currentPage.querySelector(".hero .model-left");
+    const currentRight = currentPage.querySelector(".hero .model-right");
+    const nextText = nextPage.querySelector(".details");
+    const portofolio = document.querySelector(".portofolio");
+
+    const tl = new TimelineMax();
+
+    tl.fromTo(currentLeft, 0.3, { y: "-10%" }, { y: "-100%" }).fromTo(
+      currentRight,
+      0.3,
+      { y: "10%" },
+      { y: "-100%" },
+      "-=0.2"
+    );
+  }
+}
+
+init();
