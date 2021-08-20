58d225647138a6ed7603752c480b6dde033fc6a3
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2021-08-20 10:43:50 +0100

    hamburger opens and closes

--- app.js
+++ app.js
@@ -50,8 +50,83 @@ function init() {
         { opacity: 0, pointerEvents: "none" },
         { opacity: 1, pointerEvents: "all" },
         "-=0.6"
-      );
+      )
+      .fromTo(nextLeft, 0.3, { y: "-100%" }, { y: "-10%" }, "-=0.6")
+      .fromTo(nextRight, 0.3, { y: "-100%" }, { y: "10%" }, "-=0.8")
+
+      .fromTo(nextText, 0.3, { opacity: 0, y: 0 }, { opacity: 1, y: 0 })
+      .set(nextLeft, { clearProps: "all" })
+      .set(nextRight, { clearProps: "all" });
+
+    current = pageNumber;
   }
+
+  document.addEventListener("wheel", throttle(scrollChange, 1500));
+  document.addEventListener("touchmove", throttle(scrollChange, 1500));
+
+  function switchDots(dotNumber) {
+    const activeDot = document.querySelectorAll(".slide")[dotNumber];
+    slides.forEach((slide) => {
+      slide.classList.remove("active");
+    });
+    activeDot.classList.add("active");
+  }
+
+  function scrollChange(e) {
+    if (e.deltaY > 0) {
+      scrollSlide += 1;
+    } else {
+      scrollSlide -= 1;
+    }
+
+    if (scrollSlide > 2) {
+      scrollSlide = 0;
+    }
+    if (scrollSlide < 0) {
+      scrollSlide = 2;
+    }
+    switchDots(scrollSlide);
+    nextSlide(scrollSlide);
+    console.log(scrollSlide);
+  }
+
+  const hamburger = document.querySelector(".menu");
+  const hamburgerLines = document.querySelectorAll(".menu line");
+  const navOpen = document.querySelector(".nav-open");
+  const contact = document.querySelector(".contact");
+  const social = document.querySelector(".social");
+  const logo = document.querySelector(".logo");
+
+  const tl = new TimelineMax({ paused: true, reversed: true });
+
+  tl.to(navOpen, 0.5, { y: 0 })
+    .fromTo(contact, 0.5, { opacity: 0, y: 10 }, { opacity: 1, y: 0 }, "-=0.1")
+    .fromTo(social, 0.5, { opacity: 0, y: 10 }, { opacity: 1, y: 0 }, "-=0.5")
+    .fromTo(logo, 0.2, { color: "white" }, { color: "black" }, "-=1")
+    .fromTo(
+      hamburgerLines,
+      0.2,
+      { stroke: "white" },
+      { stroke: "black" },
+      "-=1"
+    );
+
+  hamburger.addEventListener("click", () => {
+    tl.reversed() ? tl.play() : tl.reverse();
+  });
+}
+
+function throttle(func, limit) {
+  let inThrottle;
+  return function () {
+    const args = arguments;
+    const context = this;
+    if (!inThrottle) {
+      func.apply(context, args);
+      inThrottle = true;
+      setTimeout(() => (inThrottle = false), limit);
+    }
+  };
 }
 
 init();
